1a6cd86280fa86de03a0ac1bb31bb41789ab20d5
victorleungtw <victorleungtw@gmail.com>
2014-10-09 14:15:41 +0800

    finish reject test

--- src/underbar.js
+++ src/underbar.js
@@ -99,6 +99,15 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+
+    _.filter(collection, function(item, index){
+      if (test.call(item, index)){
+        result.push(item);
+      };
+    });
+
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
