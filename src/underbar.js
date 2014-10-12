c4b857b256ed187744f7a175449d0da19ffd9316
victorleungtw <victorleungtw@gmail.com>
2014-10-12 23:18:09 +0800

    implement throttle function

--- src/underbar.js
+++ src/underbar.js
@@ -523,7 +523,42 @@ var _ = {};
   // during a given window of time.
   //
   // See the Underbar readme for details.
+
+  _.now = Date.now || function() {
+    return new Date().getTime();
+  };
+
+
   _.throttle = function(func, wait) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    var options = {};
+    var later = function() {
+      previous = options.leading === false ? 0 : _.now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
+    return function() {
+      var now = _.now();
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
   };
 
 }).call(this);
